51d7dd41a5fd34b4a9140c67d000a987ca96ae36
Matt Weinstein <mattw412@gmail.com>
2023-05-29 12:08:28 -0400

    build: adds 'open' story

--- src/src/components/PageHeader/PageHeader.tsx
+++ src/src/components/PageHeader/PageHeader.tsx
@@ -4,17 +4,15 @@ import { NavigationMenuBar } from "../NavigationMenuBar/NavigationMenuBar";
 import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 
 export interface PageHeaderProps {
-  text: string;
+  open?: boolean;
 }
 
-export const PageHeader: React.FC<PageHeaderProps> = ({ text }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  console.log(isOpen);
+export const PageHeader: React.FC<PageHeaderProps> = ({open}) => {
+  const [isOpen, setIsOpen] = useState(open ?? false);
+
 
   const openMenuBar = (e: any) => {
     e.preventDefault();
-
-    console.log("hello");
     setIsOpen(!isOpen);
   };
 
@@ -23,7 +21,7 @@ export const PageHeader: React.FC<PageHeaderProps> = ({ text }) => {
       <div>LOGO</div>
       <nav className="flex justify-between gap-x-8 items-center relative">
         <NavigationItem text="Onboarding" link="#" />
-        <NavigationItem text="Funding Round" />
+        <NavigationItem text="Funding Round" link="#"/>
         <NavigationItem
           text="Developers"
           onClick={openMenuBar}
@@ -38,7 +36,7 @@ export const PageHeader: React.FC<PageHeaderProps> = ({ text }) => {
             <AiFillCaretUp className="text-black-400 inline-block align-baseline my-auto" />
           )}
         </NavigationItem>
-        <button className="rounded-xl bg-green-400 border border-black p-1 px-6">
+        <button className="rounded-xl bg-green-400 border border-black p-1 px-6 active:bg-green-800 hover:bg-green-200 ">
           Login
         </button>
       </nav>
